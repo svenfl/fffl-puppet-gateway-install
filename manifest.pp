36f1a177851da2305a21ca85e9465f0ee006269a
rubo77 <github@r.z11.de>
2017-03-28 15:27:18 +0200

    fix indentions

--- manifest.pp
+++ manifest.pp
@@ -2,7 +2,7 @@ class { 'ffnord::params':
   router_id => "10.116.$$$.$$$",  # The id of this router, probably the ipv4 address
                                   # of the mesh device of the providing community
   icvpn_as => "65525",# The as of the providing community
-  wan_devices => ['eth0'],         # An array of devices which should be in the wan zone
+  wan_devices => ['eth0'],        # An array of devices which should be in the wan zone
 
   wmem_default => 87380,          # Define the default socket send buffer
   wmem_max     => 12582912,       # Define the maximum socket send buffer
@@ -19,23 +19,21 @@ class { 'ffnord::params':
 
 # You can repeat this mesh block for every community you support
 ffnord::mesh { 'mesh_ffki':
-        mesh_name => "Freifunk Kiel"
-      , mesh_code => "ffki"
-      , mesh_as => "65525"
-      , mesh_mac  => "de:ad:be:ef:ff:06"
-      , vpn_mac   => "de:ad:be:ff:ff:06"
-      , mesh_ipv6 => "fda1:384a:74de:4242::ff06/64"
-      , mesh_ipv4  => "10.116.184.1/17"
-      , range_ipv4 => "10.116.0.0/16"
-      , mesh_mtu     => "1280"
-      , mesh_peerings    => "/root/mesh_peerings.yaml"
-
-      , fastd_secret => "/root/fastd_secret.key"
-      , fastd_port   => 11235
-      , fastd_peers_git => 'git://git.freifunk.in-kiel.de/fastd-peer-keys.git'
-
-      , dhcp_ranges => ['10.116.$$$.2 10.116.$$$.254']
-      , dns_servers => ['10.116.$$$.$$$']               # should be the same as $router_id
+    mesh_name => "Freifunk Kiel"
+  , mesh_code => "ffki"
+  , mesh_as => "65525"
+  , mesh_mac  => "de:ad:be:ef:ff:06"
+  , vpn_mac   => "de:ad:be:ff:ff:06"
+  , mesh_ipv6 => "fda1:384a:74de:4242::ff06/64"
+  , mesh_ipv4  => "10.116.184.1/17"
+  , range_ipv4 => "10.116.0.0/16"
+  , mesh_mtu     => "1280"
+  , mesh_peerings    => "/root/mesh_peerings.yaml"
+  , fastd_secret => "/root/fastd_secret.key"
+  , fastd_port   => 11235
+  , fastd_peers_git => 'git://git.freifunk.in-kiel.de/fastd-peer-keys.git'
+  , dhcp_ranges => ['10.116.$$$.2 10.116.$$$.254']
+  , dns_servers => ['10.116.$$$.$$$']               # should be the same as $router_id
 }
 
 class {
