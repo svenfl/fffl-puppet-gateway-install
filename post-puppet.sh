a82ed2be97393cabeee16d431ed5e1f184eafb48
Ruben Barkow <rubo77@users.noreply.github.com>
2017-05-02 21:32:19 +0200

    conntrack_max is set in puppet already to 32768

--- post-puppet.sh
+++ post-puppet.sh
@@ -27,37 +27,6 @@ IP6PREFIX=fda1:384a:74de:4242
 #  nameserver 8.8.8.8
 #EOF
 
-mv /etc/radvd.conf /etc/radvd.conf.bak
-cat >> /etc/radvd.conf << EOF
-# managed for interface br-$TLD
-interface br-$TLD
-{
- AdvSendAdvert on;
- AdvDefaultLifetime 0; # Here
- IgnoreIfMissing on;
- MaxRtrAdvInterval 200;
-
- prefix $IP6PREFIX:0000:0000:0000:0000/64
- {
-   AdvPreferredLifetime 14400; # Here
-   AdvValidLifetime 86400; # Here
- };
-
- RDNSS $IP6PREFIX::ff0$VPN_NUMBER
- {
- };
-
- route fc00::/7  # this block
- {
-   AdvRouteLifetime 1200;
- };
-};
-EOF
-cp /etc/radvd.conf /etc/radvd.conf.d/interface-br-$TLD.conf
-
-# set conntrack_max higher so more connections are possible:
-/sbin/sysctl -w net.netfilter.nf_conntrack_max=32768 && echo net.ipv4.netfilter.ip_conntrack_max = 32768 >> /etc/sysctl.conf
-
 # increase the hop penalty
 echo "60">/sys/class/net/bat-$TLD/mesh/hop_penalty
 
