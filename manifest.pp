18f03d09ae709e4d7eb6a98a380eaedd94c9abed
rubo77 <github@r.z11.de>
2017-05-31 11:45:28 +0200

    add unattended-upgrades

--- manifest.pp
+++ manifest.pp
@@ -76,6 +76,6 @@ class {
 
 # Useful packages
 package {
-  ['vim','tcpdump','dnsutils','realpath','screen','htop','mlocate','tig','tmux','sshguard']:
+  ['vim','tcpdump','dnsutils','realpath','screen','htop','mlocate','tig','unattended-upgrades','tmux','sshguard']:
      ensure => installed;
 }
